a1ad7b39b7f21a3011b6db25a2f17a43892b0061
Matthew Kuo <matthewkuo92@gmail.com>
2015-12-03 03:40:08 -0800

    Showing rating

--- main.js
+++ main.js
@@ -1,32 +1,73 @@
+/* global oauthSignature */
 (function() {
-	// var $ratingBox = $('.res-rating');
 
-	function acquireRating(name, address, resultCallback) {
-		var parameters = {
+	function acquireRating(name, address, cb) {
+
+		var params = {
 			term: name,
 			location: address,
-			limit: 1
-		};
+			limit: 1,
+			oauth_consumer_key: 'WY4Tg4568H1-rentOE5hmQ',
+			oauth_token: 'Ua_W7GZogS6oFUePDR6LCGaILp5Ld7Vm',
+			oauth_signature_method: 'HMAC-SHA1',
+			oauth_timestamp: new Date().getTime(),
+			oauth_nonce: _createNonce()
+		}
 
-		var request = $.get('http://api.yelp.com/v2/search'+$.param(parameters));
+		var consumerSecret = 'bd1r1BZHG9Uved8RUgyhc6wxuuk',
+				tokenSecret = 'rxTXis-pgki7uSiff9GFfbU2ZjU';
 
-		request.success(function(result) {
-			resultCallback(result.businesses[0]);
+		params.oauth_signature = oauthSignature.generate(
+			'GET',
+			'https://api.yelp.com/v2/search',
+			params,
+			consumerSecret,
+			tokenSecret,
+			{ encodeSignature: false}
+		);
+
+		$.ajax({
+			type: 'GET',
+			url: 'https://api.yelp.com/v2/search',
+			data: params,
+			success: function(results) {
+				cb(results.businesses[0]);
+			}
 		});
 
-		request.error(function(result) {
-			console.log(result);
-		});
+	}
+
+	function addYelpRating() {
+		var address = _getOwnText($('.res-main-address-text')),
+				restaurantName = $('.res-name span[itemprop=name]').text().trim();
+
+		acquireRating(restaurantName, address, function(result) {
+			var $ratingBox = $('.res-rating'),
+					$newImage = $('<img>');
+
+			$newImage.attr('src', result.rating_img_url_small);
+
+			$ratingBox.append($newImage);
+		})
+	}
+
+	function _getOwnText($obj) {
+		return $obj.clone().children().remove().end().text().trim();
+	}
+
+	function _createNonce() {
+		var text = '';
+		var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+		for( var i = 0; i < 32; i++ ) {
+			text += possible.charAt(Math.floor(Math.random() * possible.length));
+		}
+
+		return text;
 	}
 
 	$(function() {
-		address = $('.res-main-address-text')
-			.clone()
-			.children()
-			.remove()
-			.end()
-			.text()
-			.trim();
+		addYelpRating();
 	});
 
 })();
